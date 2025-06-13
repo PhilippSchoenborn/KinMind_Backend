0bbaec74e165d97a73d6b175a557526f791a4257
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-06-13 23:00:14 +0200

    Add auth_app base files and migrations

---
+++ auth_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'fullname', 'email', 'is_active', 'is_staff')
+    search_fields = ('fullname', 'email')
---
+++ auth_app/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    fullname = models.CharField(max_length=150)
+    email = models.EmailField(unique=True)
+    # username is required by AbstractUser, but we use email for login
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'fullname']
+
+    def __str__(self):
+        return self.fullname or self.email
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
