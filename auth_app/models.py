43c147422dde949c99da26bd290f442627f12300
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-06-15 17:12:43 +0200

    PEP8: Add blank lines and improve formatting in custom User model

--- auth_app/models.py
+++ auth_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 class User(AbstractUser):
     fullname = models.CharField(max_length=150)
     email = models.EmailField(unique=True)
