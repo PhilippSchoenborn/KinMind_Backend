2b87e6738b48269d80a55412dcb1f628d1e57c99
PhilippSchoenborn <155528704+PhilippSchoenborn@users.noreply.github.com>
2025-06-15 17:13:53 +0200

    PEP8: Add blank lines and remove unused imports in permissions

--- kanban_app/api/permissions.py
+++ kanban_app/api/permissions.py
@@ -1,15 +1,28 @@
-from rest_framework.permissions import BasePermission, SAFE_METHODS
+# Standardbibliothek
+# (keine)
+
+# Drittanbieter
+from rest_framework.permissions import BasePermission
+
 
 class IsBoardOwnerOrMember(BasePermission):
+    """Permission: User must be board owner or member."""
+
     def has_object_permission(self, request, view, obj):
         user = request.user
         return user == obj.owner or user in obj.members.all()
 
+
 class IsTaskBoardMember(BasePermission):
+    """Permission: User must be board owner or member for a task."""
+
     def has_object_permission(self, request, view, obj):
         user = request.user
         return user in obj.board.members.all() or user == obj.board.owner
 
+
 class IsCommentAuthor(BasePermission):
+    """Permission: User must be the author of the comment."""
+
     def has_object_permission(self, request, view, obj):
         return request.user == obj.author
